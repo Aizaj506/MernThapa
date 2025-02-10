edcf5933af240ac933699682ea9e6f03750fbdac
Aizaj Samani <aizaj506@gmail.com>
2025-02-10 12:58:48 +0530

    use-model is created

---
+++ server/models/user-model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+
+    phone: {
+        type: Number,
+        required: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+
+    isAdmin: {
+        type: String,
+        default: false,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
