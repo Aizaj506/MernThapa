50abacf4776c59fc341a02aba4c76515ae803c57
Aizaj Samani <aizaj506@gmail.com>
2025-02-10 11:51:50 +0530

    Securing Private Data With dotenv

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose  = require('mongoose')
 
-const URI = "mongodb+srv://dbAjaj:dbAjaj786@cluster0.slhaw.mongodb.net/";
+const URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try {
